ecbdf731fcc62e4f41cdcb3082530c074e581acc
Marilia Macedo <maritamisguemacedo@gmail.com>
2021-02-02 01:05:33 +0000

    feat: New File about Types

---
+++ src/main/kotlin/basico/TiposKotlin.kt
@@ -0,0 +1,16 @@
+package basico
+
+fun main(){
+    val num1: Byte = 127
+    val num2: Short = 32767
+    val num3: Int = 2_147_483_647
+    val num4: Long = 9_223_372_036_854_775_807
+
+    val num5: Double = 3.14
+    val num6: Float = 3.14f
+
+    val num7: Char = 'K'
+    val num8: Boolean = true
+
+    println(2.5 is Double)
+}
